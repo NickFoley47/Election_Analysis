dd42d2fc1104ca4d8de61845ee0c6670142dfe78
Nick Foley <90662291+NickFoley47@users.noreply.github.com>
2022-05-04 22:23:16 -0500

    Fixed print placement

--- PyPoll.py
+++ PyPoll.py
@@ -66,6 +66,9 @@ for candidate_name in candidate_votes:
     votes = candidate_votes[candidate_name]
     # 3. Calculate the percentage of votes.
     vote_percentage = float(votes) / float(total_votes) * 100
+
+    # Print out each candidate's name, vote count, and percentage of
+    # votes to the terminal.
     print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
 # Determine winning vote count and candidate
@@ -78,9 +81,7 @@ for candidate_name in candidate_votes:
         # 3. Set the winning_candidate equal to the candidate's name.
         winning_candidate = candidate_name
 
-    # To do: print out each candidate's name, vote count, and percentage of
-    # votes to the terminal.
-    
+   
 
 winning_candidate_summary = (
 f"-------------------------\n"
@@ -88,4 +89,5 @@ f"Winner: {winning_candidate}\n"
 f"Winning Vote Count: {winning_count:,}\n"
 f"Winning Percentage: {winning_percentage:.1f}%\n"
 f"-------------------------\n")
+
 print(winning_candidate_summary)
